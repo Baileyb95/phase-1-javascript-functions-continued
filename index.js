3a275de37a8bd198e0581fa03b1dd166f25be5c6
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-08-04 17:33:49 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`)
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(work = "go to the office"){
+    return (`This Monday, I will ${work}.`)
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(wrap="*"){
+    return function (adjective = "highlight"){
+            return `You are ${wrap}${adjective}${wrap}!`
+    }
+}
+wrapAdjective("!!!");
